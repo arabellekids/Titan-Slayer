66c8f05067f9ada6eff5772b629986ef16fafe94
Aaron Smith <arabellekids@gmail.com>
2019-07-03 16:03:40 -0400

    Made interacting

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    public float radius = 3;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviour {
     public NavMeshAgent agent;
     public Camera cam;
     public Animator anim;
-    public GameObject focus;
+    public Interactable focus;
+
+    bool hasInteracted = false;
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
@@ -15,6 +17,18 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(focus != null)
+        {
+            if (Vector3.Distance(transform.position, focus.transform.position) < focus.radius - 0.8 && !hasInteracted)
+            {
+                Debug.Log("Interacting");
+                hasInteracted = true;
+
+                Vector3 lookAt = new Vector3(focus.transform.position.x - transform.position.x, 0, focus.transform.position.z - transform.position.z);
+                transform.forward = lookAt;
+            }
+        }
+        
         if (Input.GetMouseButton(0))
         {
             RaycastHit hit;
@@ -22,20 +36,37 @@ public class PlayerMovement : MonoBehaviour {
 
             if (Physics.Raycast(ray, out hit))
             {
+                Defocus();
                 agent.SetDestination(hit.point);
             }
         }
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButtonDown(1))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit) && hit.collider.tag == "Interactable")
             {
-                agent.SetDestination(hit.point);
+                SetFocus(hit.collider.GetComponent<Interactable>());
             }
         }
         var speedPercent = agent.velocity.magnitude / agent.speed;
         anim.SetFloat("Speed", speedPercent);
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        agent.stoppingDistance = newFocus.radius;
+        focus = newFocus;
+        agent.SetDestination(focus.transform.position);
+        hasInteracted = false;
+
+    }
+
+    void Defocus()
+    {
+        focus = null;
+        agent.stoppingDistance = 0;
+        hasInteracted = false;
     }
 }
